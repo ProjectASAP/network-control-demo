a2a2959e087a9bd7229e3bf44f3a736929f87413
Eric S. Wang <eswang1202@gmail.com>
2025-11-05 19:52:09 -0500

    Simple prototype of controller. Takes incoming task info and creates assignments. Does not make PromQL queries or export output yet.

---
+++ solver_experimental/main.py
@@ -0,0 +1,104 @@
+import os
+import yaml
+import time
+import requests
+import argparse
+import datetime
+import numpy as np
+import logging
+from itertools import combinations
+from collections import deque
+
+# import urllib3
+from loguru import logger
+import pulp
+from typing import Dict
+import threading
+import subprocess
+import concurrent.futures
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+
+# import similarity_scores
+# from prometheus_api_client import PrometheusConnect
+# from classes.config import Config
+# from classes.QueryLatencyExporter import QueryLatencyExporter
+# from promql_utilities.query_results.classes import QueryResult, QueryResultAcrossTime
+# from promql_utilities.query_results.serializers import SerializerFactory
+
+from scheduler.entities import RunningTask, Task, NetworkTopology
+from scheduler.load_info import load_nodes, load_edges, load_tasks, build_task_graph
+from scheduler.solver import TaskScheduler
+
+
+def main(args: argparse.Namespace):
+    print("Loading network information and initializing solver...")
+    nodes = load_nodes(args.node_path)
+    edges = load_edges(args.edge_path)
+    network = NetworkTopology(nodes.values(), edges.values())
+
+    # Find shortest path between nodes.
+    paths = {}
+    for n_i, n_j in combinations(network.nodes, 2):
+        if network.has_path(n_i, n_j):
+            paths[(n_i, n_j)] = [network.find_shortest_path(n_i, n_j)]
+
+    print("Loading task request information...")
+    tasks = load_tasks(args.task_path)
+    task_graph = build_task_graph(tasks)
+    task_queue = deque(sorted(tasks.values(), key=lambda task: task.arrival_offset_s))
+
+    solver = TaskScheduler(network=network)
+    start_time = time.time()
+
+    # Mapping between task id and running task.
+    running_tasks: dict[str, RunningTask] = {}
+    unassigned_tasks: dict[str, Task] = {}
+    while running_tasks or task_queue:
+        time.sleep(args.interval)
+        curr_offset = (time.time() - start_time) * 100 
+        print(curr_offset)
+
+        # Filter out finished tasks. For now, don't account for solver time and variable finish times.
+        running_tasks = {task_id: rt for task_id, rt in running_tasks.items() if curr_offset - rt.start_time_s >= rt.task.duration_s}
+
+        arrived_tasks: dict[str, Task] = {}
+        # Schedule newly arrived tasks.
+        while task_queue:
+            task = task_queue[0]
+            if task.arrival_offset_s < curr_offset:
+                arrived_tasks[task.task_id] = task
+                task_queue.popleft()
+            else:
+                break
+        assignments, leftover_tasks, objective_value, status_code = solver.solve(
+            tasks=arrived_tasks | unassigned_tasks,
+            task_graph=task_graph,
+            running_tasks=running_tasks,
+            paths=paths
+        )
+        unassigned_tasks = leftover_tasks
+        running_tasks.update(assignments)
+
+        if pulp.LpStatus[status_code] == 'Optimal':
+            print("Optimal Assignment:")
+            for task, node in sorted(assignments.items()):
+                print(f"  {task} -> {node}")
+            
+            display_obj_value = f"{objective_value:.2f}" if objective_value is not None else "N/A"
+            print(f"\nObjective Value: {display_obj_value}")
+        else:
+            print("No optimal assignment found.")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Network demo controller.")
+    # parser.add_argument("--config-file", type=str, required=True)
+    parser.add_argument("--node-path", type=str, required=True)
+    parser.add_argument("--edge-path", type=str, required=True)
+    parser.add_argument("--task-path", type=str, required=True)
+    parser.add_argument("--task-communication-path", type=str, required=True)
+    parser.add_argument("--interval", type=float, default=10.0)
+
+    args = parser.parse_args()
+    main(args)
