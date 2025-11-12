368d0aa9be83e1fd363cdb93886417694bc004e8
Eric S. Wang <eswang1202@gmail.com>
2025-11-12 18:33:51 -0500

    Test querying rust engine and logging.

--- solver_experimental/main.py
+++ solver_experimental/main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import yaml
 import time
 import requests
@@ -38,8 +39,11 @@ QUERIES = [
 
 
 def main(args: argparse.Namespace):
-    print("Loading network information and initializing solver...")
+    logger.info("Loading network information and initializing solver...")
+
+    logger.debug(f"Node path: {args.node_path}")
     nodes = load_nodes(args.node_path)
+    logger.debug(f"Edge path: {args.edge_path}")
     edges = load_edges(args.edge_path)
     network = NetworkTopology(nodes.values(), edges.values())
 
@@ -49,7 +53,7 @@ def main(args: argparse.Namespace):
         if network.has_path(n_i, n_j):
             paths[(n_i, n_j)] = [network.find_shortest_path(n_i, n_j)]
 
-    print("Loading task request information...")
+    logger.info("Loading task request information...")
     tasks = load_tasks(args.task_path)
     task_graph = build_task_graph(tasks)
     task_queue = deque(sorted(tasks.values(), key=lambda task: task.arrival_offset_s))
@@ -63,21 +67,23 @@ def main(args: argparse.Namespace):
     while task_queue:
         time.sleep(args.interval)
         curr_offset = (time.time() - start_time) * 100 
+        logger.debug(f"Current time offset: {curr_offset:.2f} ms")
 
         for query in QUERIES:
             try:
                 params = {
                     'query': query
                 }
-                response = requests.get("http://localhost:8088", params=params)
+                response = requests.get(args.server_url, params=params)
                 response.raise_for_status()
                 data = response.json()
             except requests.RequestException as e:
-                print(f"Could not fetch telemetry information: {e}")
+                logger.debug(f"Could not fetch telemetry information: {e}")
                 continue
 
         # Filter out finished tasks. For now, don't account for solver time and variable finish times.
         running_tasks = {task_id: rt for task_id, rt in running_tasks.items() if curr_offset - rt.start_time_s >= rt.task.duration_s}
+        logger.debug(f"Currently running tasks: {list(running_tasks.keys())}")
 
         arrived_tasks: dict[str, Task] = {}
         # Schedule newly arrived tasks.
@@ -88,25 +94,33 @@ def main(args: argparse.Namespace):
                 task_queue.popleft()
             else:
                 break
+        logger.debug(f"Arrived tasks: {list(arrived_tasks.keys())}")
+        logger.debug(f"Unassigned tasks from previous rounds: {list(unassigned_tasks.keys())}")
+        
         tasks_to_schedule = arrived_tasks | unassigned_tasks
+
+        logger.info(f"Scheduling {len(tasks_to_schedule)} tasks...")
         assignments, leftover_tasks, objective_value, status_code = solver.solve(
             tasks=tasks_to_schedule,
             task_graph=task_graph,
             running_tasks=running_tasks,
             paths=paths
         )
+        logger.debug(f"Solver status: {pulp.LpStatus[status_code]}")
+
         unassigned_tasks = leftover_tasks
         running_tasks.update(assignments)
 
-        if pulp.LpStatus[status_code] == 'Optimal':
-            print("Optimal Assignment:")
-            for task, node in sorted(assignments.items()):
-                print(f"  {task} -> {node}")
-            
+        logger.info(f"Number of unassigned tasks after scheduling: {len(unassigned_tasks)}")
+        if pulp.LpStatus[status_code] == 'Optimal' and assignments:
+            assignment_repr = "Assignment: "
+            for task, rt in sorted(assignments.items()):
+                assignment_repr += f"{task} -> {rt.node_id}, "
+            logger.info(assignment_repr.rstrip(", "))
             display_obj_value = f"{objective_value:.2f}" if objective_value is not None else "N/A"
-            print(f"\nObjective Value: {display_obj_value}")
+            logger.debug(f"Objective Value: {display_obj_value}")
         else:
-            print("No optimal assignment found.")
+            logger.info("Could not assign tasks.")
 
 
 if __name__ == "__main__":
@@ -117,6 +131,11 @@ if __name__ == "__main__":
     parser.add_argument("--task-path", type=str, required=True)
     # parser.add_argument("--task-communication-path", type=str, required=True)
     parser.add_argument("--interval", type=float, default=10.0)
+    parser.add_argument("--server-url", type=str, default="http://localhost:8088/api/v1")
+    parser.add_argument("--log-level", type=str, default="INFO")
 
     args = parser.parse_args()
+
+    logger.remove()
+    logger.add(sys.stderr, level=args.log_level)
     main(args)
