ed8baf62787048c6d7a0a4caaa188694457ae9bd
GordonYuanyc <gordonyuanyc@gmail.com>
2026-01-19 22:47:20 -0700

    update kll_quantiles.rs to have lock on each kll separately

--- single_node_server/network-control-server/src/metrics/kll_quantiles.rs
+++ single_node_server/network-control-server/src/metrics/kll_quantiles.rs
@@ -1,105 +1,32 @@
-use sketchlib_rust::{
-    Hydra, KLL, SketchInput,
-    common::input::{HydraCounter, HydraQuery},
-};
-
-use super::MetricField;
-use super::key::split_key;
+use sketchlib_rust::{KLL, SketchInput};
+use std::sync::RwLock;
 
 #[derive(Default)]
 pub(super) struct MetricQuantiles {
-    pub(super) cpu_cores: KLL,
-    pub(super) memory_gb: KLL,
-    pub(super) network_mbps: KLL,
+    pub(super) cpu_cores: RwLock<KLL>,
+    pub(super) memory_gb: RwLock<KLL>,
+    pub(super) network_mbps: RwLock<KLL>,
 }
 
 impl MetricQuantiles {
     pub(super) fn insert_samples(&mut self, cpu_value: f64, memory_value: f64, network_value: f64) {
-        self.cpu_cores
-            .update(&SketchInput::F64(cpu_value))
+        let mut cpu = match self.cpu_cores.write() {
+            Ok(c) => c,
+            Err(p) => p.into_inner(),
+        };
+        cpu.update(&SketchInput::F64(cpu_value))
             .expect("cpu_cores values should be numeric");
-        self.memory_gb
-            .update(&SketchInput::F64(memory_value))
-            .expect("memory_gb values should be numeric");
-        self.network_mbps
-            .update(&SketchInput::F64(network_value))
-            .expect("network_mbps values should be numeric");
-    }
-}
-
-// struct MetricCdfs {
-//     cpu_cores: CDF,
-//     memory_gb: CDF,
-//     network_mbps: CDF,
-// }
-
-// impl MetricCdfs {
-//     fn from_sketches(sketches: MetricKll) -> Self {
-//         Self {
-//             cpu_cores: sketches.cpu_cores.cdf(),
-//             memory_gb: sketches.memory_gb.cdf(),
-//             network_mbps: sketches.network_mbps.cdf(),
-//         }
-//     }
-
-//     fn query_percentile(&self, field: MetricField, percent: f64) -> Option<f64> {
-//         if !(0.0..=100.0).contains(&percent) {
-//             return None;
-//         }
-//         let quantile = percent / 100.0;
-//         match field {
-//             MetricField::CpuCores => Some(self.cpu_cores.query(quantile)),
-//             MetricField::MemoryGb => Some(self.memory_gb.query(quantile)),
-//             MetricField::NetworkMbps => Some(self.network_mbps.query(quantile)),
-//         }
-//     }
-// }
-
-pub(super) struct MetricHydra {
-    cpu_quantile: Hydra,
-    mem_quantile: Hydra,
-    net_quantile: Hydra,
-}
-
-impl MetricHydra {
-    pub(super) fn new() -> Self {
-        let kll_template = HydraCounter::KLL(KLL::default());
-
-        Self {
-            cpu_quantile: Hydra::with_dimensions(3, 64, kll_template.clone()),
-            mem_quantile: Hydra::with_dimensions(3, 64, kll_template.clone()),
-            net_quantile: Hydra::with_dimensions(3, 64, kll_template),
-        }
-    }
-
-    pub(super) fn update(
-        &mut self,
-        key: &str,
-        cpu_value: f64,
-        memory_value: f64,
-        network_value: f64,
-    ) {
-        let cpu_input = SketchInput::F64(cpu_value);
-        let memory_input = SketchInput::F64(memory_value);
-        let network_input = SketchInput::F64(network_value);
-
-        self.cpu_quantile.update(key, &cpu_input, None);
-        self.mem_quantile.update(key, &memory_input, None);
-        self.net_quantile.update(key, &network_input, None);
-    }
-
-    pub(super) fn query_quantile(
-        &self,
-        field: MetricField,
-        key: &str,
-        quantile: f64,
-    ) -> Option<f64> {
-        let parts = split_key(key)?;
-        let query = HydraQuery::Quantile(quantile);
-        Some(match field {
-            MetricField::CpuCores => self.cpu_quantile.query_key(parts, &query),
-            MetricField::MemoryGb => self.mem_quantile.query_key(parts, &query),
-            MetricField::NetworkMbps => self.net_quantile.query_key(parts, &query),
-        })
+        let mut mem = match self.memory_gb.write() {
+            Ok(m) => m,
+            Err(p) => p.into_inner(),
+        };
+        mem.update(&SketchInput::F64(memory_value))
+            .expect("cpu_cores values should be numeric");
+        let mut net = match self.network_mbps.write() {
+            Ok(n) => n,
+            Err(p) => p.into_inner(),
+        };
+        net.update(&SketchInput::F64(network_value))
+            .expect("cpu_cores values should be numeric");
     }
 }
