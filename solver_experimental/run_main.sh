981225f3e5586a5d031cc448d372c8008b9694a8
GordonYuanyc <gordonyuanyc@gmail.com>
2026-01-22 01:05:35 -0700

    use the jsonl file for setup

--- solver_experimental/run_main.sh
+++ solver_experimental/run_main.sh
@@ -7,9 +7,9 @@ EMULATOR_PID=$!
 trap 'kill "$EMULATOR_PID"' EXIT
 
 uv run main.py \
-    --node-path "dummy_data/nodes.csv" \
-    --edge-path "dummy_data/edges.csv" \
-    --task-path "dummy_data/tasks.csv" \
+    --node-path "dummy_data/nodes.jsonl" \
+    --edge-path "dummy_data/edges.jsonl" \
+    --task-path "dummy_data/tasks.jsonl" \
     --interval 1.0 \
     --query-manager-config "configs/sample.yml" \
     --log-level "INFO"
